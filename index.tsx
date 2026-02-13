0d060902048e115ac8ac45e5f4620f820d074f1a
ncrespo <crespo-nicolas@hotmail.com>
2026-02-13 14:06:44 -0300

    feat: Remove Rajdhani font and optimize CSS; update HTML for Google Fonts integration

--- index.tsx
+++ index.tsx
@@ -8,11 +8,6 @@ import '@fontsource/lato/700.css';
 import '@fontsource/playfair-display/400.css';
 import '@fontsource/playfair-display/600.css';
 import '@fontsource/playfair-display/700.css';
-import '@fontsource/rajdhani/300.css';
-import '@fontsource/rajdhani/400.css';
-import '@fontsource/rajdhani/500.css';
-import '@fontsource/rajdhani/600.css';
-import '@fontsource/rajdhani/700.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
